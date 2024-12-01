7d02dcdc35177171a81933b2f3a9b2f515882fd1
Mohamed Araby <mohamedaraby1296@gmail.com>
2024-12-02 01:24:12 +0200

    Update order_chart_screen.dart

--- lib/features/orders/presentation/order_chart/order_chart_screen.dart
+++ lib/features/orders/presentation/order_chart/order_chart_screen.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:task/di/di.dart';
+import 'package:task/features/orders/logic/entity/order_filtered.dart';
 import 'package:task/features/orders/presentation/order_chart/manager/order_chart_state.dart';
 
 import '../order_chart/manager/order_chart_manager.dart';
 
 class OrderChartScreen extends StatelessWidget {
   const OrderChartScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
+    TooltipBehavior _tooltipBehavior = TooltipBehavior(enable: true);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Orders Chart'),
       ),
       body: BlocProvider(
         create: (context) => locator<OrderChartManager>(),
         child: BlocBuilder<OrderChartManager, OrderChartState>(
           builder: (context, state) {
             return (switch (state) {
-              (OrderChartDataState state) => Text(
-                  state.orders
-                      .map((e) => "${e.date} : ${e.orders.length}")
-                      .join(" - "),
+              (OrderChartDataState state) => SfCircularChart(
+                  tooltipBehavior: _tooltipBehavior,
+                  title: const ChartTitle(text: 'Number of Order VS Date'),
+                  legend: const Legend(
+                      isVisible: true,
+                      overflowMode: LegendItemOverflowMode.wrap),
+                  series: <CircularSeries>[
+                    PieSeries<OrderFilterResponse, String>(
+                      dataSource: state.orders,
+                      xValueMapper: (OrderFilterResponse response, _) =>
+                          response.date.year.toString() +
+                          "-" +
+                          response.date.month.toString(),
+                      yValueMapper: (OrderFilterResponse response, _) =>
+                          response.orders.length,
+                      dataLabelSettings:
+                          const DataLabelSettings(isVisible: true),
+                    )
+                  ],
                 ),
+              //   Text(
+              //   state.orders
+              //       .map((e) => "${e.date} : ${e.orders.length}")
+              //       .join(" - "),
+              //   style: const TextStyle(
+              //     color: Colors.black,
+              //   ),
+              // ),
               (OrderChartFailure state) => Text(state.errorMessage.toString()),
               (_) => const Center(child: CircularProgressIndicator()),
             });
